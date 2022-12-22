e6b13f5c60634a734a95f18993cd91fc116e4f94
gunjan-sachdeva <58691180+gunjan-sachdeva@users.noreply.github.com>
2022-12-22 12:48:55 +0530

    welcome

---
+++ src/components/Gallery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Gallery = () => {
+  return (
+    <div>
+      <h1>My Gallery Component</h1>
+      <p>welcome EverYone</p>
+    </div>
+  )
+}
+
+export default Gallery
